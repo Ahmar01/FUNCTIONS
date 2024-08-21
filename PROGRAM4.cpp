bdc5521ed61c17ad48a00e6e1e683f4604c6e2be
Ahmar Rasheed <146799779+Ahmar01@users.noreply.github.com>
2024-08-21 10:11:25 +0500

    Add files via upload

---
+++ PROGRAM4.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ 
+using namespace std;
+
+int num (int a){
+   int p ,s;
+   p = a-1;
+   s=a+1;
+   cout<<p<<endl;
+   cout<<s;
+}
+int main(){
+	int a;
+	cout<<"enter a number";
+	cin>>a;
+    num(a);
+
+}
